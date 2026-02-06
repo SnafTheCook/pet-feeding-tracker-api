044e6efa99a9c14cbd1431a7413598f67377a11f
pawelles <153841838+SnafTheCook@users.noreply.github.com>
2026-02-06 16:09:16 +0100

    Create FeedingServiceTests.cs

---
+++ DidWeFeedTheCatToday.Tests/Services/FeedingServiceTests.cs
@@ -0,0 +1,24 @@
+﻿using DidWeFeedTheCatToday.Data;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DidWeFeedTheCatToday.Tests.Services
+{
+    public class FeedingServiceTests
+    {
+        private AppDbContext GetDbContext()
+        {
+            var options = new DbContextOptionsBuilder<AppDbContext>()
+                .UseInMemoryDatabase(databaseName: Guid.NewGuid().ToString())
+                .Options;
+
+            return new AppDbContext(options);
+        }
+
+
+    }
+}
