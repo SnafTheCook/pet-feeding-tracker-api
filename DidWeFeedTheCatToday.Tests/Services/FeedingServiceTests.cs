193409c5cd91d81ceb4e2144878378be1fffb892
pawelles <153841838+SnafTheCook@users.noreply.github.com>
2026-02-07 15:44:40 +0100

    Add GetFeedings tests

--- DidWeFeedTheCatToday.Tests/Services/FeedingServiceTests.cs
+++ DidWeFeedTheCatToday.Tests/Services/FeedingServiceTests.cs
@@ -1,4 +1,9 @@
 ﻿using DidWeFeedTheCatToday.Data;
+using DidWeFeedTheCatToday.DTOs.Feedings;
+using DidWeFeedTheCatToday.DTOs.PetFeedings;
+using DidWeFeedTheCatToday.Entities;
+using DidWeFeedTheCatToday.Services.Implementations;
+using FluentAssertions;
 using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
@@ -19,6 +24,57 @@ namespace DidWeFeedTheCatToday.Tests.Services
             return new AppDbContext(options);
         }
 
+        [Fact]
+        public async Task GetFeedingsAsync_WhenFeedingsExist_ReturnsAllFeedingsAsDtos()
+        {
+            using var context = GetDbContext();
+            var service = new FeedingService(context);
 
+            var testPet = new Pet { Name = "Meowstarion" };
+            var testFeeding1 = new Feeding { PetId = testPet.Id, FeedingTime = DateTime.UtcNow.AddMinutes(-5) };
+            var testFeeding2 = new Feeding { PetId = testPet.Id, FeedingTime = DateTime.UtcNow };
+
+            context.Pets.Add(testPet);
+            context.Feedings.AddRange(testFeeding1, testFeeding2);
+            await context.SaveChangesAsync();
+
+            var result = await service.GetFeedingsAsync();
+
+            result.Should().NotBeNull();
+            result.GetType().Should().BeAssignableTo<List<GetFeedingDTO>>();
+            result.Count().Should().Be(2);
+        }
+
+        [Fact]
+        public async Task GetFeedingByIdAsync_WhenFeedingExists_ReturnsCorrectDto()
+        {
+            using var context = GetDbContext();
+            var service = new FeedingService(context);
+
+            var testPet = new Pet { Name = "Meowstarion" };
+            context.Pets.Add(testPet);
+
+            var testFeeding = new Feeding { PetId = testPet.Id, FeedingTime = DateTime.UtcNow };
+            context.Feedings.Add(testFeeding);
+
+            await context.SaveChangesAsync();
+
+            var result = await service.GetFeedingByIdAsync(testFeeding.Id);
+
+            result.Should().NotBeNull();
+            result.GetType().Should().BeAssignableTo<GetFeedingDTO>();
+            result.PetId.Should().Be(testPet.Id);
+        }
+
+        [Fact]
+        public async Task GetFeedingByIdAsync_WhenFeedingDoesNotExist_ReturnsNull()
+        {
+            using var context = GetDbContext();
+            var service = new FeedingService(context);
+
+            var result = await service.GetFeedingByIdAsync(9999);
+
+            result.Should().BeNull();
+        }
     }
 }
