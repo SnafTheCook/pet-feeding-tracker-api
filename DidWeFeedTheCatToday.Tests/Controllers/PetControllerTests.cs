1c236de141fe5cbc79b122d31638fdecb9db1d55
pawelles <153841838+SnafTheCook@users.noreply.github.com>
2026-02-10 21:17:52 +0100

    Add GetPetById tests

--- DidWeFeedTheCatToday.Tests/Controllers/PetControllerTests.cs
+++ DidWeFeedTheCatToday.Tests/Controllers/PetControllerTests.cs
@@ -1,4 +1,11 @@
-﻿using System;
+﻿using DidWeFeedTheCatToday.Common;
+using DidWeFeedTheCatToday.Controllers;
+using DidWeFeedTheCatToday.DTOs.Pets;
+using DidWeFeedTheCatToday.Services.Interfaces;
+using FluentAssertions;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +13,51 @@ using System.Threading.Tasks;
 
 namespace DidWeFeedTheCatToday.Tests.Controllers
 {
-    internal class PetControllerTests
+    public class PetControllerTests
     {
+        private readonly Mock<IPetService> _mockPetService;
+        private readonly PetController _petController;
+
+        public PetControllerTests()
+        {
+            _mockPetService = new Mock<IPetService>();
+            _petController = new PetController(_mockPetService.Object);
+        }
+
+        [Fact]
+        public async Task GetPetById_WhenPetExists_ReturnsOkWithData()
+        {
+            var petId = 1;
+            var petDto = new GetPetDTO { Id = petId, Name = "Meowstarion" };
+
+            _mockPetService
+                .Setup(setup => setup.GetPetByIdAsync(petId))
+                .ReturnsAsync(petDto);
+
+            var result = await _petController.GetPetById(petId);
+
+            var okResult = result.Result.Should().BeOfType<OkObjectResult>().Subject;
+            var apiResponse = okResult.Value.Should().BeOfType<ApiResponse<GetPetDTO>>().Subject;
+
+            apiResponse.Success.Should().BeTrue();
+            apiResponse.Data.Should().BeEquivalentTo(petDto);
+        }
+
+        [Fact]
+        public async Task GetPetById_WhenPetDoesntExist_ReturnsNotFound()
+        {
+            var petId = 9999;
+
+            _mockPetService
+                .Setup(setup => setup.GetPetByIdAsync(petId))
+                .ReturnsAsync((GetPetDTO?) null);
+
+            var result = await _petController.GetPetById(petId);
+            var resultNotFound = result.Result.Should().BeOfType<NotFoundObjectResult>().Subject;
+            var apiResponse = resultNotFound.Value.Should().BeOfType<ApiResponse<GetPetDTO>>().Subject;
+
+            apiResponse.Success.Should().BeFalse();
+            apiResponse.Error.Should().Be("No Pet found under index.");
+        }
     }
 }
