c1608438909ea4392be63e0fc7c78ec9a03cd2f7
pawelles <153841838+SnafTheCook@users.noreply.github.com>
2026-02-12 14:39:45 +0100

    Add GetPetsWithFeedingsById tests

--- DidWeFeedTheCatToday.Tests/Controllers/PetFeedingQueryControllerTests.cs
+++ DidWeFeedTheCatToday.Tests/Controllers/PetFeedingQueryControllerTests.cs
@@ -26,66 +26,74 @@ namespace DidWeFeedTheCatToday.Tests.Controllers
             _controller = new PetFeedingQueryController(_mockPetFeedingQueryService.Object);
         }
 
+        private List<GetPetFeedingDTO> GetTestDataSetup()
+        {
+            return new List<GetPetFeedingDTO>
+            {
+                new GetPetFeedingDTO
+                {
+                    Id = 1,
+                    Name = "Meowstarion ",
+                    FeedingTimes = new List<GetFeedingDTO>
+                    {
+                        new GetFeedingDTO
+                        {
+                            Id = 1,
+                            FeedingTime = DateTime.UtcNow,
+                            PetId = 1,
+                        },
+                        new GetFeedingDTO
+                        {
+                            Id = 2,
+                            FeedingTime = DateTime.UtcNow,
+                            PetId = 1,
+                        },
+                        new GetFeedingDTO
+                        {
+                            Id = 3,
+                            FeedingTime = DateTime.UtcNow,
+                            PetId = 1,
+                        }
+                    }
+                },
+
+                new GetPetFeedingDTO
+                {
+                    Id = 2,
+                    Name = "Katlach",
+                    FeedingTimes = new List<GetFeedingDTO>
+                    {
+                        new GetFeedingDTO
+                        {
+                            Id = 4,
+                            FeedingTime = DateTime.UtcNow,
+                            PetId = 2,
+                        },
+                        new GetFeedingDTO
+                        {
+                            Id = 5,
+                            FeedingTime = DateTime.UtcNow,
+                            PetId = 2,
+                        },
+                        new GetFeedingDTO
+                        {
+                            Id = 6,
+                            FeedingTime = DateTime.UtcNow,
+                            PetId = 2,
+                        }
+                    }
+                }
+            };
+        }
+
         [Fact]
         public async Task GetPetsWithFeedings_WhenPetsExist_ReturnsOKWithData()
         {
-            var testPet1 = new GetPetFeedingDTO
-            {
-                Id = 1,
-                Name = "Meowstarion ",
-                FeedingTimes = new List<GetFeedingDTO>
-                {
-                    new GetFeedingDTO
-                    {
-                        Id = 1,
-                        FeedingTime = DateTime.UtcNow,
-                        PetId = 1,
-                    },
-                    new GetFeedingDTO
-                    {
-                        Id = 2,
-                        FeedingTime = DateTime.UtcNow,
-                        PetId = 1,
-                    },
-                    new GetFeedingDTO
-                    {
-                        Id = 3,
-                        FeedingTime = DateTime.UtcNow,
-                        PetId = 1,
-                    }
-                }
-            };
-
-            var testPet2 = new GetPetFeedingDTO
-            {
-                Id = 2,
-                Name = "Katlach",
-                FeedingTimes = new List<GetFeedingDTO>
-                {
-                    new GetFeedingDTO
-                    {
-                        Id = 4,
-                        FeedingTime = DateTime.UtcNow,
-                        PetId = 2,
-                    },
-                    new GetFeedingDTO
-                    {
-                        Id = 5,
-                        FeedingTime = DateTime.UtcNow,
-                        PetId = 2,
-                    },
-                    new GetFeedingDTO
-                    {
-                        Id = 6,
-                        FeedingTime = DateTime.UtcNow,
-                        PetId = 2,
-                    }
-                }
-            };
+            var testPets = GetTestDataSetup();
 
             _mockPetFeedingQueryService
                 .Setup(setup => setup.GetAllPetFeedingsAsync())
-                .ReturnsAsync(new List<GetPetFeedingDTO> { testPet1, testPet2 });
+                .ReturnsAsync(testPets);
 
             var result = await _controller.GetPetsWithFeedings();
 
@@ -96,6 +104,44 @@ namespace DidWeFeedTheCatToday.Tests.Controllers
             responseData.Should().HaveCount(2);
             responseData[0].FeedingTimes.Should().NotBeEmpty();
             responseData[1].FeedingTimes[0].Id.Should().Be(4);
+        }
+
+        [Fact]
+        public async Task GetPetsWithFeedingsById_WhenPetFound_ReturnsOkWithData()
+        {
+            var testPet = GetTestDataSetup()[0];
+            int testId = 1;
+
+            _mockPetFeedingQueryService
+                .Setup(setup => setup.GetPetFeedingsByIdAsync(testId))
+                .ReturnsAsync(testPet);
+
+            var result = await _controller.GetPetsWithFeedingsById(testId);
+
+            var okResult = result.Result.Should().BeOfType<OkObjectResult>().Subject;
+            var apiResponse = okResult.Value.Should().BeOfType<ApiResponse<GetPetFeedingDTO>>().Subject;
+
+            apiResponse.Data.Should().NotBeNull();
+            apiResponse.Data.Id.Should().Be(testId);
+            apiResponse.Data.FeedingTimes.Should().NotBeNull();
+        }
+
+        [Fact]
+        public async Task GetPetsWithFeedingsById_WhenPetNotFound_ReturnsNotFound()
+        {
+            int testId = 1;
+
+            _mockPetFeedingQueryService
+                .Setup(setup => setup.GetPetFeedingsByIdAsync(testId))
+                .ReturnsAsync((GetPetFeedingDTO?)null);
+
+            var result = await _controller.GetPetsWithFeedingsById(testId);
+
+            var notFoundResult = result.Result.Should().BeOfType<NotFoundObjectResult>().Subject;
+            var apiResponse = notFoundResult.Value.Should().BeOfType<ApiResponse<GetPetFeedingDTO>>().Subject;
+
+            apiResponse.Data.Should().BeNull();
+            apiResponse.Error.Should().Be("Pet and it's feeding times were not found.");
         }
     }
 }
