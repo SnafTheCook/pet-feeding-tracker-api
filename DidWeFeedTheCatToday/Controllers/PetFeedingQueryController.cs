201263df7296c13c48f7ae81ae106a08a2f7d97a
pawelles <153841838+SnafTheCook@users.noreply.github.com>
2026-02-25 16:22:57 +0100

    Add summaries to methods
    Add summaries to methods in PetFeedingQueryController.cs

--- DidWeFeedTheCatToday/Controllers/PetFeedingQueryController.cs
+++ DidWeFeedTheCatToday/Controllers/PetFeedingQueryController.cs
@@ -12,7 +12,10 @@ namespace DidWeFeedTheCatToday.Controllers
     [ApiController]
     public class PetFeedingQueryController(IPetFeedingQueryService petFeedingQueryService) : ControllerBase
     {
-
+        /// <summary>
+        /// Retrieve all pets and their respective feeding histories.
+        /// </summary>
+        /// <returns>API response containing a collection of <see cref="GetPetFeedingDTO"/> and their nested feeding records.</returns>
         [Authorize(Roles = nameof(Roles.Admin) + "," + nameof(Roles.Parent))] //for now authorization is used only here for development
         [HttpGet]
         public async Task<ActionResult<ApiResponse<IEnumerable<GetPetFeedingDTO>>>> GetPetsWithFeedings()
@@ -22,6 +25,11 @@ namespace DidWeFeedTheCatToday.Controllers
             return Ok(ApiResponse<IEnumerable<GetPetFeedingDTO>>.Ok(result));
         }
 
+        /// <summary>
+        /// Retrieve a specific pet and its feeding history by the unique id.
+        /// </summary>
+        /// <param name="id">Unique id of the pet.</param>
+        /// <returns>API response containing a List of feedings records of that pet.</returns>
         [HttpGet("{id}")]
         public async Task<ActionResult<ApiResponse<GetPetFeedingDTO>>> GetPetsWithFeedingsById(int id)
         {
