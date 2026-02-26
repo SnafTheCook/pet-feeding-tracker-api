1a7048af20e49d70b47ea2828173be8a1c60e03f
pawelles <153841838+SnafTheCook@users.noreply.github.com>
2026-02-26 14:48:53 +0100

    Update CommandPetDto.cs with data annotations

--- DidWeFeedTheCatToday.Shared/DTOs/Pets/CommandPetDTO.cs
+++ DidWeFeedTheCatToday.Shared/DTOs/Pets/CommandPetDTO.cs
@@ -1,10 +1,15 @@
-﻿namespace DidWeFeedTheCatToday.Shared.DTOs.Pets
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DidWeFeedTheCatToday.Shared.DTOs.Pets
 {
     public class CommandPetDTO
     {
-        //public int Id { get; set; }
+        [Required(ErrorMessage = "Pet's name is required.")]
+        [StringLength(30, ErrorMessage = "Pet's name is too long.")]
         public required string Name { get; set; }
+        [Range(0, 200, ErrorMessage = "Age must be between 0 and 200.")]
         public int? Age { get; set; }
+        [StringLength(500, ErrorMessage = "Additional information is too long.")]
         public string? AdditionalInformation { get; set; }
     }
 }
