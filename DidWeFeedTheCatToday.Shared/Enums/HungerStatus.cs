81144de8d0a314f87a3fffaca706fb3f04343852
pawelles <153841838+SnafTheCook@users.noreply.github.com>
2026-03-01 13:13:24 +0100

    Add hunger status with enum, update query linq

---
+++ DidWeFeedTheCatToday.Shared/Enums/HungerStatus.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DidWeFeedTheCatToday.Shared.Enums
+{
+    public enum HungerStatus
+    {
+        Full,       //Fed less than 2 hours ago
+        Content,    //Fed between 2 and 5 hours ago
+        Hungry,     //Fed 5-10 hours ago
+        Starving    //Fed 10+ hours ago (or 10+ minutes ago for cats)
+    }
+}
