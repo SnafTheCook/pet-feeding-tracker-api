ff1694a43eb57b64ce67b0f38f7c0b13b9e04408
pawelles <153841838+SnafTheCook@users.noreply.github.com>
2026-03-11 16:53:29 +0100

    Add summaries to PUT and DELETE methods

--- DidWeFeedTheCatToday/Services/Implementations/FeedingService.cs
+++ DidWeFeedTheCatToday/Services/Implementations/FeedingService.cs
@@ -35,7 +35,11 @@ namespace DidWeFeedTheCatToday.Services.Implementations
 
             return FeedingToGetFeedingDTO(feeding);
         }
-
+        /// <summary>
+        /// Persists a new feeding if a target pet exists.
+        /// </summary>
+        /// <param name="feeding">Feeding data</param>
+        /// <returns>a <see cref="GetFeedingDTO"/> on success. Returns <see langword="null"/> if pet not found.</returns>
         public async Task<GetFeedingDTO?> AddFeedingAsync(PostFeedingDTO feeding)
         {
             if (!await context.Pets.AnyAsync(p => p.Id == feeding.PetId))
@@ -54,7 +58,11 @@ namespace DidWeFeedTheCatToday.Services.Implementations
 
             return FeedingToGetFeedingDTO(request);
         }
-
+        /// <summary>
+        /// Removes a feeding from history.
+        /// </summary>
+        /// <param name="id">Unique identifier of the feeding.</param>
+        /// <returns>a <see langword="bool"/> indicating success or failure.</returns>
         public async Task<bool> DeleteFeedingAsync(int id)
         {
             var feeding = await context.Feedings.FindAsync(id);
