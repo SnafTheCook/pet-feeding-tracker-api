574b1c32500bf529034729bd0f7b38423d8264ab
pawelles <153841838+SnafTheCook@users.noreply.github.com>
2026-03-16 20:02:34 +0100

    Add LoginAsync happy path test

--- DidWeFeedTheCatToday.Tests/Services/AuthServiceTests.cs
+++ DidWeFeedTheCatToday.Tests/Services/AuthServiceTests.cs
@@ -5,6 +5,7 @@ using DidWeFeedTheCatToday.Services.Implementations;
 using DidWeFeedTheCatToday.Services.Interfaces;
 using DidWeFeedTheCatToday.Shared.DTOs.Auth;
 using FluentAssertions;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
@@ -51,6 +52,29 @@ namespace DidWeFeedTheCatToday.Tests.Services
             var result = await service.RegisterAsync(request);
 
             result.Should().BeNull();
+        }
+
+        [Fact]
+        public async Task LoginAsync_WithValidCredentials_ReturnsToken()
+        {
+            using var db = GetDbContext();
+            var password = "secretTestPassword123!";
+            var username = "TestUser";
+            var user = new User { Id = Guid.NewGuid(), Username = username };
+
+            user.PasswordHash = new PasswordHasher<User>().HashPassword(user, password);
+
+            db.Users.Add(user);
+            await db.SaveChangesAsync();
+
+            var service = new AuthService(db, _options, _mockRequestContext.Object, _mockLogger.Object);
+            var loginDto = new UserDTO { Username = username,  Password = password };
+
+            var result = await service.LoginAsync(loginDto);
+
+            result.Should().NotBeNull();
+            result.AccessToken.Should().NotBeNullOrEmpty();
+            result.RefreshToken.Should().NotBeNullOrEmpty();
         }
     }
 }
