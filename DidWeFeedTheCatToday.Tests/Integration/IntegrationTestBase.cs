24ffbe46e8a8b5be087b8fb8d383426f91644e2a
pawelles <153841838+SnafTheCook@users.noreply.github.com>
2026-05-10 17:28:28 +0200

    test: add WebApplicationFactory for integration

---
+++ DidWeFeedTheCatToday.Tests/Integration/IntegrationTestBase.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.VisualStudio.TestPlatform.TestHost;
+using Notification.Domain;
+
+namespace DidWeFeedTheCatToday.Tests.Integration
+{
+    public class IntegrationTestBase : IClassFixture<WebApplicationFactory<Program>>
+    {
+        protected readonly HttpClient _httpClient;
+
+        public IntegrationTestBase(WebApplicationFactory<Program> webApplicationFactory)
+        {
+            _httpClient = webApplicationFactory.CreateClient();
+        }
+    }
+}
