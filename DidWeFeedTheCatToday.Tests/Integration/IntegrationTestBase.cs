20d689e6eb3d6e00ed65e4bd89d36b3f628b7cd8
pawelles <153841838+SnafTheCook@users.noreply.github.com>
2026-05-11 06:15:40 +0200

    test: use sqlite in-memory for integration tests

--- DidWeFeedTheCatToday.Tests/Integration/IntegrationTestBase.cs
+++ DidWeFeedTheCatToday.Tests/Integration/IntegrationTestBase.cs
@@ -5,6 +5,9 @@ using System.Text;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc.Testing;
 using DidWeFeedTheCatToday;
+using Microsoft.EntityFrameworkCore;
+using DidWeFeedTheCatToday.Data;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace DidWeFeedTheCatToday.Tests.Integration
 {
@@ -14,6 +17,27 @@ namespace DidWeFeedTheCatToday.Tests.Integration
 
         public IntegrationTestBase(WebApplicationFactory<Program> webApplicationFactory)
         {
+            var testFactory = webApplicationFactory.WithWebHostBuilder(builder =>
+            {
+                builder.ConfigureServices(services =>
+                {
+                    var descriptor = services.SingleOrDefault(d => d.ServiceType == typeof(DbContextOptions<AppDbContext>));
+                    if (descriptor != null) services.Remove(descriptor);
+
+                    services.AddDbContext<AppDbContext>(options =>
+                    {
+                        options.UseSqlite("Data Source=:memory:");
+                    });
+
+                    var sp = services.BuildServiceProvider();
+                    using var scope = sp.CreateScope();
+                    var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+
+                    db.Database.OpenConnection();
+                    db.Database.EnsureCreated();
+                });
+            });
+
             _httpClient = webApplicationFactory.CreateClient();
         }
     }
