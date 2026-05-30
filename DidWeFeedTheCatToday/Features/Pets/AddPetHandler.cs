3c949dc7f89dc481c23304ce02b54c482ec2b3d9
pawelles <153841838+SnafTheCook@users.noreply.github.com>
2026-05-30 13:39:10 +0200

    refactor: implement AddPetHandler logic

---
+++ DidWeFeedTheCatToday/Features/Pets/AddPetHandler.cs
@@ -0,0 +1,24 @@
+﻿using DidWeFeedTheCatToday.Data;
+using DidWeFeedTheCatToday.Entities;
+using DidWeFeedTheCatToday.Shared.DTOs.Pets;
+using MediatR;
+
+namespace DidWeFeedTheCatToday.Features.Pets
+{
+    public class AddPetHandler(AppDbContext context) : IRequestHandler<AddPetCommand, GetPetDTO>
+    {
+        public async Task<GetPetDTO> Handle(AddPetCommand request, CancellationToken cancellationToken)
+        {
+            var pet = new Pet
+            {
+                Name = request.PetDto.Name,
+                Age = request.PetDto.Age
+            };
+
+            context.Pets.Add(pet);
+            await context.SaveChangesAsync();
+
+            return new GetPetDTO { Id = pet.Id, Name = pet.Name };
+        }
+    }
+}
