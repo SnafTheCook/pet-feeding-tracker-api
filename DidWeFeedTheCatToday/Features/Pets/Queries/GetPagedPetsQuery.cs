0c2ef118f821c76e807c275c6a996702070ddaf0
pawelles <153841838+SnafTheCook@users.noreply.github.com>
2026-06-07 09:07:01 +0200

    refactor: define GetPagedPetsQuery

---
+++ DidWeFeedTheCatToday/Features/Pets/Queries/GetPagedPetsQuery.cs
@@ -0,0 +1,12 @@
+﻿using DidWeFeedTheCatToday.Shared.Common;
+using DidWeFeedTheCatToday.Shared.DTOs.Pets;
+using MediatR;
+
+namespace DidWeFeedTheCatToday.Features.Pets.Queries
+{
+    public record GetPagedPetsQuery(
+        int Page,
+        int PageSize,
+        string? SearchTerm,
+        string? SortBy) : IRequest<PagedResult<GetPetDTO>>;
+}
