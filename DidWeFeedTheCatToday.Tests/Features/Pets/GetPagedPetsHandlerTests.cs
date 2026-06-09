c7da97f9cf55cf00b5f4e21660dcb65abd464943
pawelles <153841838+SnafTheCook@users.noreply.github.com>
2026-06-09 08:50:58 +0200

    test: setup infrastructure for GetPagedPetsHandler

---
+++ DidWeFeedTheCatToday.Tests/Features/Pets/GetPagedPetsHandlerTests.cs
@@ -0,0 +1,34 @@
+﻿using DidWeFeedTheCatToday.Data;
+using DidWeFeedTheCatToday.Data.Interceptors;
+using DidWeFeedTheCatToday.Features.Pets.Queries;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Caching.Memory;
+
+namespace DidWeFeedTheCatToday.Tests.Features.Pets
+{
+    public class GetPagedPetsHandlerTests : IDisposable
+    {
+        private readonly AppDbContext _context;
+        private readonly IMemoryCache _cache;
+        private readonly GetPagedPetsHandler _handler;
+        private readonly UpdateAuditableInterceptor _auditingInterceptor = new();
+        public GetPagedPetsHandlerTests()
+        {
+            var options = new DbContextOptionsBuilder<AppDbContext>()
+                .UseInMemoryDatabase(databaseName: Guid.NewGuid().ToString())
+                .AddInterceptors(_auditingInterceptor)
+                .Options;
+
+            _context = new AppDbContext(options);
+
+            _cache = new MemoryCache(new MemoryCacheOptions());
+            _handler = new GetPagedPetsHandler(_context, _cache);
+        }
+
+        public void Dispose()
+        {
+            _context.Dispose();
+            _cache.Dispose();
+        }
+    }
+}
