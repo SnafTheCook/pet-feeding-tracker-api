32a3b3806a3ac3b10b79158f73845348804dec04
pawelles <153841838+SnafTheCook@users.noreply.github.com>
2026-06-10 07:45:58 +0200

    test: migrate pagination logic tests to MediatR handler

--- DidWeFeedTheCatToday.Tests/Features/Pets/GetPagedPetsHandlerTests.cs
+++ DidWeFeedTheCatToday.Tests/Features/Pets/GetPagedPetsHandlerTests.cs
@@ -1,6 +1,8 @@
 ﻿using DidWeFeedTheCatToday.Data;
 using DidWeFeedTheCatToday.Data.Interceptors;
+using DidWeFeedTheCatToday.Entities;
 using DidWeFeedTheCatToday.Features.Pets.Queries;
+using FluentAssertions;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Caching.Memory;
 
@@ -23,6 +25,24 @@ namespace DidWeFeedTheCatToday.Tests.Features.Pets
 
             _cache = new MemoryCache(new MemoryCacheOptions());
             _handler = new GetPagedPetsHandler(_context, _cache);
+        }
+
+        [Fact]
+        public async Task Handle_WhenRequestingSecondPage_ReturnsCorrectSlice()
+        {
+            for (int i = 1; i <= 7; i++)
+            {
+                _context.Pets.Add(new Pet { Name = $"Pet {i}" });
+            }
+
+            await _context.SaveChangesAsync();
+
+            var query = new GetPagedPetsQuery(Page: 2, PageSize: 5, SearchTerm: null, SortBy: "name");
+
+            var result = await _handler.Handle(query, CancellationToken.None);
+
+            result.Items.Should().HaveCount(2);
+            result.TotalCount.Should().Be(7);
         }
 
         public void Dispose()
