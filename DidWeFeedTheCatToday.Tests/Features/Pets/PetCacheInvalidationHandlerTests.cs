7ba73846a9eb27359b4a97d6c4379fd04e0d6e5d
pawelles <153841838+SnafTheCook@users.noreply.github.com>
2026-06-11 05:46:32 +0200

    test: implement cache invalidation handler tests

---
+++ DidWeFeedTheCatToday.Tests/Features/Pets/PetCacheInvalidationHandlerTests.cs
@@ -0,0 +1,45 @@
+﻿using DidWeFeedTheCatToday.Features.Pets;
+using DidWeFeedTheCatToday.Features.Pets.Notifications;
+using FluentAssertions;
+using Microsoft.Extensions.Caching.Memory;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DidWeFeedTheCatToday.Tests.Features.Pets
+{
+    public class PetCacheInvalidationHandlerTests : IDisposable
+    {
+        private readonly IMemoryCache _cache;
+        private readonly PetCacheInvalidationHandler _handler;
+
+        public PetCacheInvalidationHandlerTests()
+        {
+            _cache = new MemoryCache(new MemoryCacheOptions());
+            _handler = new PetCacheInvalidationHandler();
+
+            PetCacheHelper.InvalidateCache();
+        }
+
+        [Fact]
+        public async Task Handle_WhenPetChanged_InvalidatesCache()
+        {
+            var cacheKey = "test_key";
+            var options = new MemoryCacheEntryOptions()
+                .AddExpirationToken(PetCacheHelper.GetExpirationToken());
+
+            _cache.Set(cacheKey, "some_data", options);
+            _cache.Get(cacheKey).Should().NotBeNull();
+
+            await _handler.Handle(new PetChangedNotification(), CancellationToken.None);
+
+            _cache.Get(cacheKey).Should().BeNull();
+        }
+        public void Dispose()
+        {
+            _cache.Dispose();
+        }
+    }
+}
