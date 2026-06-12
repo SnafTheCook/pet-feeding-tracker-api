90b91c3ea3ab53031cef4f4526472343ff705c75
pawelles <153841838+SnafTheCook@users.noreply.github.com>
2026-06-12 05:58:26 +0200

    feat: implement MediatR logging behavior

---
+++ DidWeFeedTheCatToday/Features/Common/Behaviors/LoggingBehavior.cs
@@ -0,0 +1,22 @@
+﻿using MediatR;
+using System.Diagnostics;
+
+namespace DidWeFeedTheCatToday.Features.Common.Behaviors
+{
+    public class LoggingBehavior<TRequest, TResponse>(ILogger<LoggingBehavior<TRequest, TResponse>> logger)
+        : IPipelineBehavior<TRequest, TResponse> where TRequest : notnull
+    {
+        public async Task<TResponse> Handle(TRequest request, RequestHandlerDelegate<TResponse> next, CancellationToken ct)
+        {
+            var requestName = typeof(TRequest).Name;
+            logger.LogInformation("Starting request {RequestName}", requestName);
+
+            var timer = Stopwatch.StartNew();
+            var response = await next();
+            timer.Stop();
+
+            logger.LogInformation("Finished request {RequestName} in {Elapsed}ms", requestName, timer.ElapsedMilliseconds);
+            return response;
+        }
+    }
+}
