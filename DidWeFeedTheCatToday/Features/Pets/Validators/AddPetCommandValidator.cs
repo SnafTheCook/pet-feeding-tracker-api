9ede2013a8f14e4e2b8799469f8a5b4a51b8edc3
pawelles <153841838+SnafTheCook@users.noreply.github.com>
2026-06-13 12:52:19 +0200

    test: fix validation regression in MediatR pipeline

---
+++ DidWeFeedTheCatToday/Features/Pets/Validators/AddPetCommandValidator.cs
@@ -0,0 +1,18 @@
+﻿using FluentValidation;
+
+namespace DidWeFeedTheCatToday.Features.Pets.Validators
+{
+    public class AddPetCommandValidator : AbstractValidator<AddPetCommand>
+    {
+        public AddPetCommandValidator()
+        {
+            RuleFor(x => x.PetDto.Name)
+                .NotEmpty()
+                .MinimumLength(5)
+                .WithMessage("Name must be at least 5 characters long.");
+
+            RuleFor(x => x.PetDto.Age)
+                .InclusiveBetween(0, 100);
+        }
+    }
+}
