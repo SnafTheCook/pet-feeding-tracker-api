1a847c4f460fe6f6f3e2df247ab65ff7bea811e5
pawelles <153841838+SnafTheCook@users.noreply.github.com>
2026-07-15 14:56:00 +0200

    refactor: define MediatR requests for feedings

---
+++ DidWeFeedTheCatToday/Features/Feedings/FeedingRequests.cs
@@ -0,0 +1,10 @@
+﻿namespace DidWeFeedTheCatToday.Features.Feedings
+{
+    using MediatR;
+    using DidWeFeedTheCatToday.Shared.DTOs.Feedings;
+
+    public record GetFeedingsQuery : IRequest<IEnumerable<GetFeedingDTO>>;
+    public record GetFeedingByIdQuery(int Id) : IRequest<GetFeedingDTO?>;
+    public record AddFeedingCommand(PostFeedingDTO DTO) : IRequest<GetFeedingDTO?>;
+    public record DeleteFeedingCommand(int Id) : IRequest<bool>;
+}
