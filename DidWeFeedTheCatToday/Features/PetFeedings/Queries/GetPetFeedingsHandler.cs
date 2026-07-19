b39dbeaabd72df02226a5c21d7b7d059a647819c
pawelles <153841838+SnafTheCook@users.noreply.github.com>
2026-07-19 13:33:29 +0200

    refactor: implement list handler for pet feedings

--- DidWeFeedTheCatToday/Features/PetFeedings/Queries/GetPetFeedingsHandler.cs
+++ DidWeFeedTheCatToday/Features/PetFeedings/Queries/GetPetFeedingsHandler.cs
@@ -1,6 +1,31 @@
-﻿namespace DidWeFeedTheCatToday.Features.PetFeedings.Queries
+﻿using DidWeFeedTheCatToday.Data;
+using DidWeFeedTheCatToday.Shared.DTOs.Feedings;
+using DidWeFeedTheCatToday.Shared.DTOs.PetFeedings;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+
+namespace DidWeFeedTheCatToday.Features.PetFeedings.Queries
 {
-    public class GetPetFeedingsHandler
+    public class GetPetFeedingsHandler(AppDbContext context)
+    : IRequestHandler<GetPetFeedingsQuery, IEnumerable<GetPetFeedingDTO>>
     {
+        public async Task<IEnumerable<GetPetFeedingDTO>> Handle(GetPetFeedingsQuery request, CancellationToken ct)
+        {
+            return await context.Pets
+                .AsNoTracking()
+                .Include(p => p.FeedingTimes)
+                .Select(p => new GetPetFeedingDTO
+                {
+                    Id = p.Id,
+                    Name = p.Name,
+                    FeedingTimes = p.FeedingTimes.Select(f => new GetFeedingDTO
+                    {
+                        Id = f.Id,
+                        PetId = f.PetId,
+                        FeedingTime = f.FeedingTime
+                    }).ToList()
+                })
+                .ToListAsync(ct);
+        }
     }
 }
